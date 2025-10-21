d29dc5178267d6b4c596f3355a996d0f31a00ad8
LimitAbsolute <1749089175@qq.com>
2025-10-21 17:24:18 +0800

    新增数据，修改样式

--- data/commonData.js
+++ data/commonData.js
@@ -1,10 +1,20 @@
+//卡片类型
 const cardTypes = [
+    '攻击型',
+    '生产型',
+    '防御型',
+    '炸弹型',
+    '辅助型'
+];
+//稀有度
+const rarity = [
     '普通卡',
     '纪念卡',
     '生肖卡',
     '星座卡',
     '神卡'
 ];
+//标签类型
 const tagTypes = [
     'info',
     'primary',
@@ -12,3 +22,20 @@ const tagTypes = [
     'warning',
     'danger'
 ];
+//为不同稀有度设置不同颜色
+const rarityColors = {
+    '普通卡': '#909399',
+    '纪念卡': '#409EFF',
+    '生肖卡': '#67C23A',
+    '星座卡': '#E6A23C',
+    '神卡': '#F56C6C'
+};
+//转职卡片使用不同颜色
+const transferColors = {
+    'base': '#c0c0c0',
+    'transfer1': '#FFFF00',
+    'transfer2': '#FFD700',
+    'transfer3': '#FFA500',
+    'transfer4': '#FF8C00',
+    'transfer5': '#FF4500'
+};
