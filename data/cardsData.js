9225bbaf3e48962072469d01ccd7ba4381e491fb
LimitAbsolute <1749089175@qq.com>
2025-12-03 13:31:19 +0800

    更新数据

--- data/cardsData.js
+++ data/cardsData.js
@@ -119,7 +119,7 @@ const cardsData = [{
         transfer5: {
             name: '火山可乐弹',
             energy: 300,
-            info: '爆炸后在5*5范围内随机生成2个可乐炸弹'
+            info: '爆炸后在5*5范围内随机生成1-3个可乐炸弹'
         }
     },
     {
